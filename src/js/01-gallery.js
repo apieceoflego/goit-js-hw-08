e098fe23e6e4bbf1114dbbc76daf675cb4922fe9
apieceoflego <olehprychshepchuk@gmail.com>
2022-10-25 21:01:02 +0200

    HW 1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const itemOfGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+  )
+  .join('');
+
+const markupOfGallery = gallery.insertAdjacentHTML('afterbegin', itemOfGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (data) {
+  _.throttle(() => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
