1bd665cec43debb5180878d3623f346d42924c98
apieceoflego <olehprychshepchuk@gmail.com>
2022-10-27 20:33:13 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const MESSAGE_KEY = 'feedback-form-state';
+
+const inputData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextAreaAllInput, 500));
+
+populateTextArea();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(MESSAGE_KEY);
+  console.log(inputData);
+}
+
+function onTextAreaAllInput(event) {
+  inputData[event.target.name] = event.target.value;
+  localStorage.setItem(MESSAGE_KEY, JSON.stringify(inputData));
+}
+
+function populateTextArea() {
+  const savedMessage = localStorage.getItem(MESSAGE_KEY);
+  const parsedMessage = JSON.parse(savedMessage);
+  if (parsedMessage) {
+    refs.textarea.value = parsedMessage.message;
+    refs.input.value = parsedMessage.email;
+  }
+}
